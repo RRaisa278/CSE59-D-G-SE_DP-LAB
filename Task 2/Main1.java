f99f208e890025ae885fddae5ff3e4d9b4eeeab4
RRaisa278 <rahiatulraisa1618@gmail.com>
2026-06-18 16:40:11 +0600

    Update Main1.java

--- Task 2/Main1.java
+++ Task 2/Main1.java
@@ -1,1 +1,12 @@
+public class conteststats {
+    int[] scores = {85, 90, 95};
 
+    void printFirstScore() {
+        System.out.println("First score: " + scores[0]);
+    }
+
+    public static void main(String[] args) {
+        conteststats c = new conteststats();
+        c.printFirstScore();
+    }
+}
