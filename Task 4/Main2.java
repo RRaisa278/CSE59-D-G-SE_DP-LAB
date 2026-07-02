3b748ed79309dfb87b44e319a34bd7990c8da005
RRaisa278 <rahiatulraisa1618@gmail.com>
2026-07-02 17:30:05 +0600

    Update Main2.java

--- Task 4/Main2.java
+++ Task 4/Main2.java
@@ -1,1 +1,55 @@
 
+interface Printer {
+    void printDocument();
+}
+
+
+interface Scanner {
+    void scanDocument();
+}
+
+interface FaxMachine {
+    void faxDocument();
+}
+
+
+class BasicPrinter implements Printer {
+
+    @Override
+    public void printDocument() {
+        System.out.println("Printing document...");
+    }
+}
+
+
+class MultiFunctionCopier implements Printer, Scanner, FaxMachine {
+
+    @Override
+    public void printDocument() {
+        System.out.println("Printing document...");
+    }
+
+    @Override
+    public void scanDocument() {
+        System.out.println("Scanning document...");
+    }
+
+    @Override
+    public void faxDocument() {
+        System.out.println("Faxing document...");
+    }
+}
+
+
+public class Main2 {
+    public static void main(String[] args) {
+
+        BasicPrinter printer = new BasicPrinter();
+        printer.printDocument();
+
+        MultiFunctionCopier copier = new MultiFunctionCopier();
+        copier.printDocument();
+        copier.scanDocument();
+        copier.faxDocument();
+    }
+}
