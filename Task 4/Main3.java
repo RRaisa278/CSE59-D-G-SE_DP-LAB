9f5719907787cd7b26f239aaea70ce794b86ac71
RRaisa278 <rahiatulraisa1618@gmail.com>
2026-07-02 17:31:44 +0600

    Update Main3.java

--- Task 4/Main3.java
+++ Task 4/Main3.java
@@ -1,1 +1,49 @@
 
+interface MessageSender {
+    void sendMessage(String message);
+}
+
+
+class EmailSender implements MessageSender {
+
+    @Override
+    public void sendMessage(String message) {
+        System.out.println("Sending Email: " + message);
+    }
+}
+
+class SMSSender implements MessageSender {
+
+    @Override
+    public void sendMessage(String message) {
+        System.out.println("Sending SMS: " + message);
+    }
+}
+
+
+class NotificationService {
+
+    private MessageSender sender;
+
+    public NotificationService(MessageSender sender) {
+        this.sender = sender;
+    }
+
+    public void alertUser(String message) {
+        sender.sendMessage(message);
+    }
+}
+
+
+public class Main3 {
+    public static void main(String[] args) {
+
+        MessageSender email = new EmailSender();
+        NotificationService emailService = new NotificationService(email);
+        emailService.alertUser("Meeting starts at 10 AM.");
+
+        MessageSender sms = new SMSSender();
+        NotificationService smsService = new NotificationService(sms);
+        smsService.alertUser("Your OTP is 123456.");
+    }
+}
